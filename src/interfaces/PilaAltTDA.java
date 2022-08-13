6c74a2870787467d020ab97e9a659ade09d79dc5
dario-ferrari <57733048+dario-ferrari@users.noreply.github.com>
2022-08-12 21:06:45 -0300

    Update PilaAltTDA.java

--- src/interfaces/PilaAltTDA.java
+++ src/interfaces/PilaAltTDA.java
@@ -2,30 +2,28 @@ package interfaces;
 
 /**
  * @author Dario Ferrari
- * @Pila es una estructura que permite almacenar una colección de valores, eliminarlos
+ * @Pila es una estructura que permite almacenar una colecciÃ³n de valores, eliminarlos
  * y recuperarlos, con la particularidad de que el elemento que se recupera o elimina 
- * es el último que ingresó.
+ * es el Ãºltimo que ingresÃ³.
  */
 
-//Esta interfaz de pila es la que hice para el Ejercicio 2 de las Tareas en Teams. Y es usada por una Pila Estática
-//con Tope al Inicio, pero con un código distinto al de la pila estática con tope al inicio que dio la profesora.
-//Esta interfaz no tiene los métodos "tope" y "desapilar" separados, sino que junta ambos creando "topeSacar".
+//Esta interfaz no tiene los mÃ©todos "tope" y "desapilar" separados, sino que junta ambos creando "topeSacar".
 
 public interface PilaAltTDA {
 	/**@Tarea inicializa la estructura de Pila.
-	 * @Precondición ninguna.
+	 * @PrecondiciÃ³n ninguna.
 	 */
 	void inicializarPila();
 	/**@Tarea Apilar agrega un elemento. 
-	 * @Precondición la pila debe estar inicializada.
+	 * @PrecondiciÃ³n la pila debe estar inicializada.
 	 */
 	void apilar(int x);
-	/**@Tarea elimina el último elemento agregado a la pila. Junta los métodos "tope" y "desapilar".
-	 * @Precondición La estructura debe estar inicializada y no vacía.
+	/**@Tarea elimina el Ãºltimo elemento agregado a la pila. Junta los mÃ©todos "tope" y "desapilar".
+	 * @PrecondiciÃ³n La estructura debe estar inicializada y no vacÃ­a.
 	 */
 	int topeSacar();
 	/**@Tarea Indica si la pila contiene elementos o no.
-	 * @Precondición la pila debe estar inicializada.
+	 * @PrecondiciÃ³n la pila debe estar inicializada.
 	 */
 	boolean pilaVacia();
 
